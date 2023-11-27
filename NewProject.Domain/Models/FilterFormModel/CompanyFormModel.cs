866c272dd9ba18ebf9983e18d72d3f71302079bd
temitayo <tytunji29@gmail.com>
2023-11-27 22:35:50 +0100

    okay

--- NewProject.Domain/Models/FilterFormModel/CompanyFormModel.cs
+++ NewProject.Domain/Models/FilterFormModel/CompanyFormModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,43 +9,44 @@ namespace NewProject.Domain.Models.FormModel
 {
     public class CampaignFormModel : DefualtFormModel
     {
-        public string Camapagnid { get; set; }
-        public string ProductId { get; set; }
+        public string? Camapagnid { get; set; }
+        public string? ProductId { get; set; }
     }
     public class ClientFormModel : DefualtFormModel
     {
-        public string ClientId { get; set; }
+        public string? ClientId { get; set; }
     }
     public class SubscriptionFormModel : DefualtFormModel
     {
-        public string marketerId { get; set; }
-        public string tranxid { get; set; }
+        public string? marketerId { get; set; }
+        public string? tranxid { get; set; }
     }
     public class ProductFormModel : DefualtFormModel
     {
-        public string ProductId { get; set; }
-        public string MarketerId { get; set; }
+        public string? ProductId { get; set; }
+        public string? MarketerId { get; set; }
     }
     public class MarketerFormModel : DefualtFormModel
     {
-        public string MemberId { get; set; }
+        public string? MemberId { get; set; }
     }
-     public class LogFormModel : DefualtFormModel
+    public class LogFormModel : DefualtFormModel
     {
-        public string ClientId { get; set; }
-        public string marketerId { get; set; }
-        public string produtid { get; set; }
+        public string? ClientId { get; set; }
+        public string? marketerId { get; set; }
+        public string? produtid { get; set; }
     }
     public class CommissionFormModel : DefualtFormModel
     {
-        public string BenId { get; set; }
+        public string? BenId { get; set; }
     }
     public class CompanyFormModel : DefualtFormModel
     {
-        public string PinnacleId { get; set; }
+        public string? PinnacleId { get; set; }
     }
     public class DefualtFormModel
     {
+        [Required]
         public string CoyId { get; set; }
     }
 }
